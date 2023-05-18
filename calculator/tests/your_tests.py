1c18149b5869395dcba02205bd2e0cb31752503d
kguevar2 <133183585+kguevar2@users.noreply.github.com>
2023-05-17 22:24:24 -0700

    Update your_tests.py

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -3,8 +3,17 @@ from calculator_adapter import run
 
 
 ### ADD AT LEAST TWO TESTS HERE!
+# Checks that program outputs "2" for an input "8 / 4"
+assert run("8 / 4").output == "2"
 
+# Checks that program output "-2" for an input "1 - 3"
+assert run("1 - 3").output == "-2"
 
+# Checks that program outputs "0" for an input "0 * 5"
+assert run("0 * 5").output == "0"
+
+# Checks that program outputs "9" for an input "7 + 2"
+assert run("7 + 2").output == "9"
 ###
 
 print("All tests passed!")
